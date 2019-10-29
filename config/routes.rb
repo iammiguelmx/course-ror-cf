7537db72adbd5d1d6f140996877a69fdde244054
Miguel <miguelangelcamachocampos@gmail.com>
2019-10-28 23:17:52 -0600

    Created of views :tada:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :articles
+  
+   # get "/articles"
+   # post "/articles"
+   # delete "/articles"
+   # get "/articles/:id"
+   # get "/aticles/new"
+   # get "/articles/:id/edit"
+   # patch "/articles/:id"
+   # put "/articles/:id"
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
